0e356bea444cf8eca837b0afac304fa39a6c4521
Montaser <omarmontaser34@gmail.com>
2023-08-25 23:35:43 +0300

    adding Oauth Google Feature

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config() 
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+
 const app = express() ;
 
 const cookieParser = require('cookie-parser')
@@ -12,15 +15,22 @@ const connect = require('./db/connect')
 const authRoute = require('./route/auth')
 const hotelsRoute = require('./route/hotels')
 const roomsRoute = require('./route/rooms')
+const authGoogle = require('./route/oauth')
 
 //MiddleWare 
 
 app.use(express.json()) ;
 app.use(cookieParser())
+app.use(session( {secret:process.env.SESSION_SECRET, resave:true ,saveUninitialized:true }));
+app.use(passport.initialize()) ;
+app.use(passport.session()) ;
+
+
 
 app.use('/api/auth' , authRoute) ;
 app.use('/api/hotels' , hotelsRoute) ;
 app.use('/api/room' , roomsRoute) ;
+app.use('/api/oauth' , authGoogle) ;
 
 const PORT = 3000 || process.env.PORT
 
